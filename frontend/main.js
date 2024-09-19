82189f7e1169d4b2831ca807cc5e9631a087a5aa
iamdeepaksingh <deepak.singh.buzz@gmail.com>
2024-09-19 11:05:53 +0200

    Deployed storage and functions

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionAPI = "http://localhost:7071/api/GetResumeCounter";
+//const localfunctionAPI = "http://localhost:7071/api/GetResumeCounter";
+const functionAPIUrl = "https://getvisitcountersinghd.azurewebsites.net/api/GetVisitCounter?code=R-RyTIkZFEtDd3ctL7IIUu9kWaplxXP5nHT-3GVG9qJjAzFuv_Nabg%3D%3D"
 /* This is where the backend api (Azure function with bindings with CosmosDB) is connected with the frontend */
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionAPI)
+    fetch(functionAPIUrl)
         .then((response) => {
         return response.json();
         })
